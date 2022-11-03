d119a33118179150a585b2e8d871c2ba1a62f802
AmanGH009 <aman.mandal.71334@gmail.com>
2022-11-03 16:05:49 +0530

    commit

---
+++ src/components/Menu.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  flex: 1;
+`;
+const Menu = () => {
+  return <Container>Menu</Container>;
+};
+
+export default Menu;
