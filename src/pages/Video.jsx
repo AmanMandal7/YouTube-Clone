9824175280a8134638ec97e00c3a34998ba106ae
AmanGH009 <aman.mandal.71334@gmail.com>
2022-11-04 20:05:39 +0530

    pages

---
+++ src/pages/Video.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+const Video = () => {
+  return <Container>Video</Container>;
+};
+
+export default Video;
