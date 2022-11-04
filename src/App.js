42a154b10e091a3002e2802de1f45b4e0d1f9fe6
AmanGH009 <aman.mandal.71334@gmail.com>
2022-11-04 11:17:06 +0530

    dark mode

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
-import styled from 'styled-components';
+import { useState } from 'react';
+import styled, { ThemeProvider } from 'styled-components';
 import Menu from './components/Menu';
 import Navbar from './components/Navbar';
+import { darkTheme, lightTheme } from './utils/Theme';
 
 const Container = styled.div`
-  display:flex
+  display:flex;
 `
 const Main = styled.div`
-  flex:7
+  flex:7;
+  background-color: ${({ theme }) => theme.bg};
 `
 const Wrapper = styled.div``
 
 function App() {
+
+  const [darkMode, setDarkMode] = useState(true);
+
   return (
-    <Container>
-      <Menu />
-      <Main>
-        <Navbar />
-        <Wrapper>
-          video cards
-        </Wrapper>
-      </Main>
-    </Container>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Container>
+        <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
+        <Main>
+          <Navbar />
+          <Wrapper>
+            video cards
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+            <h1>land</h1>
+          </Wrapper>
+        </Main>
+      </Container>
+    </ThemeProvider>
   );
 }
 
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,11 +1,159 @@
 import React from "react";
 import styled from "styled-components";
+import YouTube from "../img/logo.png";
+import HomeIcon from "@mui/icons-material/Home";
+import ExploreOutlinedIcon from "@mui/icons-material/ExploreOutlined";
+import SubscriptionsOutlinedIcon from "@mui/icons-material/SubscriptionsOutlined";
+import VideoLibraryOutlinedIcon from "@mui/icons-material/VideoLibraryOutlined";
+import RestoreOutlinedIcon from "@mui/icons-material/RestoreOutlined";
+import LibraryMusicOutlinedIcon from "@mui/icons-material/LibraryMusicOutlined";
+import SportsBasketballOutlinedIcon from "@mui/icons-material/SportsBasketballOutlined";
+import SportsEsportsOutlinedIcon from "@mui/icons-material/SportsEsportsOutlined";
+import MovieCreationOutlinedIcon from "@mui/icons-material/MovieCreationOutlined";
+import ArticleOutlinedIcon from "@mui/icons-material/ArticleOutlined";
+import LiveTvOutlinedIcon from "@mui/icons-material/LiveTvOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import OutlinedFlagOutlinedIcon from "@mui/icons-material/OutlinedFlagOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import SettingsBrightnessOutlinedIcon from "@mui/icons-material/SettingsBrightnessOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 
 const Container = styled.div`
-  flex: 1;
+  flex: 1.3;
+  background-color: ${({ theme }) => theme.bg};
+  height: 100vh;
+  color: ${({ theme }) => theme.text};
+  position: sticky;
+  top: 0;
 `;
-const Menu = () => {
-  return <Container>Menu</Container>;
+
+const Wrapper = styled.div`
+  padding: 18px 26px;
+  font-size: 14px;
+`;
+
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  font-weight: bold;
+  margin-bottom: 25px;
+`;
+
+const Img = styled.img`
+  height: 25px;
+`;
+
+const Item = styled.div`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  gap: 20px;
+  padding: 4px 0px;
+`;
+
+const Hr = styled.hr`
+  margin: 15px 0px;
+  border: 0.5px solid ${({ theme }) => theme.soft};
+`;
+
+const Login = styled.div``;
+const Button = styled.button`
+  padding: 5px 15px;
+  background-color: transparent;
+  border: 1px solid #3ea6ff;
+  color: #3ea6ff;
+  border-radius: 3px;
+  font-weight: 500;
+  margin-top: 10px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`;
+
+const Menu = ({ darkMode, setDarkMode }) => {
+  return (
+    <Container>
+      <Wrapper>
+        <Logo>
+          <Img src={YouTube} />
+          YouTube
+        </Logo>
+        <Item>
+          <HomeIcon />
+          Home
+        </Item>
+        <Item>
+          <ExploreOutlinedIcon fontSize="small" />
+          Explore
+        </Item>
+        <Item>
+          <SubscriptionsOutlinedIcon fontSize="small" />
+          Subscriptions
+        </Item>
+        <Hr />
+        <Item>
+          <VideoLibraryOutlinedIcon fontSize="small" />
+          Library
+        </Item>
+        <Item>
+          <RestoreOutlinedIcon fontSize="small" />
+          History
+        </Item>
+        <Hr />
+        <Login>
+          Sign in to like videos, comment, and subscribe.
+          <Button>
+            <AccountCircleOutlinedIcon fontSize="small" />
+            SIGN IN
+          </Button>
+        </Login>
+        <Hr />
+        <Item>
+          <LibraryMusicOutlinedIcon fontSize="small" />
+          Music
+        </Item>
+        <Item>
+          <SportsBasketballOutlinedIcon fontSize="small" />
+          Sports
+        </Item>
+        <Item>
+          <SportsEsportsOutlinedIcon fontSize="small" />
+          Gaming
+        </Item>
+        <Item>
+          <MovieCreationOutlinedIcon fontSize="small" />
+          Movies
+        </Item>
+        <Item>
+          <ArticleOutlinedIcon fontSize="small" />
+          News
+        </Item>
+        <Item>
+          <LiveTvOutlinedIcon fontSize="small" />
+          Live
+        </Item>
+        <Hr />
+        <Item>
+          <SettingsOutlinedIcon fontSize="small" />
+          Settings
+        </Item>
+        <Item>
+          <OutlinedFlagOutlinedIcon fontSize="small" />
+          Report
+        </Item>
+        <Item>
+          <HelpOutlineOutlinedIcon fontSize="small" />
+          Help
+        </Item>
+        <Item onClick={() => setDarkMode(!darkMode)}>
+          <SettingsBrightnessOutlinedIcon fontSize="small" />
+          Light Mode
+        </Item>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default Menu;
